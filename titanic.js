76f89b94c6a285bbb658f0de4deaed4aa466bdbb
Ruslan <korus86@mail.ru>
2022-02-20 20:24:16 +0300

    v.2) added search by enter, some cosmetic, scroll depends on table's height

--- titanic.js
+++ titanic.js
@@ -1,36 +1,43 @@
-let lastPassengerIndex = 40;
+let lastPassengerIndex = 0;
 let tableBody = document.querySelector('.table-body');
 let searchButton = document.querySelector('.search__button');
+let passengersDiv = document.querySelector('.passengers');
+let search = document.querySelector('.search');
 fetch('https://raw.githubusercontent.com/altkraft/for-applicants/master/frontend/titanic/passengers.json')
-.then(response => {
-    return response.json()
-})
-.then(passengersOfTitanic => {       
-     
-    for (let i = 0; i < 40; i++) {
-        addToTitanic(passengersOfTitanic[i]);
-    }
-
-    document.addEventListener('scroll', () => {
-        if (window.innerHeight + pageYOffset >= document.body.offsetHeight - 30) {
-            if (lastPassengerIndex < passengersOfTitanic.length) {
-                addToTitanic(passengersOfTitanic[lastPassengerIndex]);
-                lastPassengerIndex++;
-            }
-        }
+    .then(response => {
+        return response.json()
     })
+    .then(passengersOfTitanic => {
 
-    searchButton.addEventListener('click', () => {
-        tableBody.innerHTML = '';
-        let searchInput = document.querySelector('.search__input');
-        let filteredPassangers = passengersOfTitanic.filter(passenger => passenger.name.toLowerCase().includes(searchInput.value.toLowerCase()))
-        for (let i = 0; i < filteredPassangers.length; i++) {
-            addToTitanic(filteredPassangers[i]);
+
+        while (passengersDiv.clientHeight + search.clientHeight <= document.body.offsetHeight) {
+            addToTitanic(passengersOfTitanic[lastPassengerIndex]);
+            lastPassengerIndex++;
         }
-        
-    })   
-    
-})
+
+        document.addEventListener('scroll', () => {
+            if (window.innerHeight + scrollY >= document.body.offsetHeight - 30) {
+                if (lastPassengerIndex < passengersOfTitanic.length) {
+                    addToTitanic(passengersOfTitanic[lastPassengerIndex]);
+                    lastPassengerIndex++;
+                }
+            }
+        })
+
+        function callSearch() {
+            searchingPassengers(passengersOfTitanic);
+        }
+
+        searchButton.addEventListener('click', callSearch);
+        window.addEventListener('keydown', event => {
+            if (event.key === 'Enter') {
+                callSearch()
+            }
+        });
+        console.log(passengersOfTitanic[1309].survived);
+        console.log(passengersOfTitanic[1].survived);
+        console.log(passengersOfTitanic[2].survived);
+    });
 
 function addToTitanic(passenger) {
     let tr = document.createElement('tr');
@@ -39,12 +46,45 @@ function addToTitanic(passenger) {
     let tdGender = document.createElement('td');
     let tdAge = document.createElement('td');
     let tdSurvived = document.createElement('td');
-    tdName.textContent = passenger.name;
-    tdGender.textContent = passenger.gender;
-    tdAge.textContent = Math.ceil(passenger.age);
-    tdSurvived.textContent = passenger.survived ? "SURVIVED" : "NOT SURVIVED";
+    tdName.textContent = printDataIsCorrupted(passenger.name);
+    tdGender.textContent = printDataIsCorrupted(passenger.gender);
+    tdAge.textContent = Math.ceil(printDataIsCorrupted(passenger.age));
+    tdSurvived.textContent = printDataIsCorrupted(passenger.survived); 
     tr.append(tdName);
     tr.append(tdGender);
     tr.append(tdAge);
-    tr.append(tdSurvived);   
+    tr.append(tdSurvived);
+}
+
+function searchingPassengers(passengersOfTitanic) {
+    let searchInput = document.querySelector('.search__input');
+    if (searchInput.value === '') {
+        search.classList.add('search__error');
+        searchInput.placeholder = 'Enter the name that you want to find...';
+    } else {
+        search.classList.remove('search__error');
+        tableBody.innerHTML = '';
+        let filteredPassangers = passengersOfTitanic.filter(passenger => passenger.name.toLowerCase().includes(searchInput.value.toLowerCase()))
+        if (filteredPassangers.length === 0) {
+            searchInput.value = '';
+            searchInput.placeholder = 'Passangers with this name - not found! Enter another name...';
+        }
+        else {
+            for (let i = 0; i < filteredPassangers.length; i++) {
+                addToTitanic(filteredPassangers[i]);
+            }
+        }
+    }
+}
+
+function printDataIsCorrupted(prop) {
+    if (prop === '' || prop === 'null' || prop === null) {
+        return prop = 'Sorry, but this data is corrupted...'
+    } else if (prop === true) {
+        return prop = 'Survived'
+    } else if (prop === false) {
+        return prop = 'Died'
+    } else {
+        return prop = prop;
+    }
 }
